d3cc9c2ba196d52c92cc8f8c761381f438aa14d2
shaniedulab <shani@edulab.in>
2024-03-18 20:00:40 +0530

    SM-rbac on post question bank

--- src/features/v1/questionBank/questionBank.route.ts
+++ src/features/v1/questionBank/questionBank.route.ts
@@ -4,7 +4,7 @@ import { IRoles } from '../user/user.types';
 import QuestionBankController from './questionBank.controller';
 const router = Router();
 
-router.post('/', Rbac([IRoles.SUPER_ADMIN]), QuestionBankController.createQuestionBank);
+router.post('/', Rbac([IRoles.SUPER_ADMIN,IRoles.SME]), QuestionBankController.createQuestionBank);
 
 router.get('/', Rbac([IRoles.SUPER_ADMIN, IRoles.SME, IRoles.PAPER_SETTER, IRoles.MODERATOR, IRoles.COE_HEAD, IRoles.COE_STAFF]), QuestionBankController.getQuestionBank);
 
