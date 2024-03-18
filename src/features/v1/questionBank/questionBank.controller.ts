7b629bd2c0422002dd61096299d8cc8589d593e0
anujedulab <anuj@edulab.in>
2024-03-18 11:42:07 +0530

    question bank sme role fetch data api added

--- src/features/v1/questionBank/questionBank.controller.ts
+++ src/features/v1/questionBank/questionBank.controller.ts
@@ -1,18 +1,33 @@
-import { NextFunction, Response,Request } from "express";
+import { NextFunction, Response, Request } from "express";
 import QuestionBankUtils from "./questionBank.utils";
 import sendSuccessResponse from "../../../middlewares/success-handler";
+import { IRoles } from "../user/user.types";
 
 export default class QuestionBankController {
 
-         static  async createQuestionBank(req:Request,res:Response,next:NextFunction){
-            try{
-                const userId = req.user.id
-                const newData = req.body.map((value) =>({ ...value, userId }));
-                const result=  await QuestionBankUtils.saveQuestionBank(newData)
-                if(result) sendSuccessResponse(req,res,{message:"Successfully stored"})
-            }catch(error){
-                next(error)
-            }
-       
+    static async createQuestionBank(req: Request, res: Response, next: NextFunction) {
+        try {
+            const userId = req.user.id
+            const newData = req.body.map((value) => ({ ...value, userId }));
+            const result = await QuestionBankUtils.saveQuestionBank(newData)
+            if (result) sendSuccessResponse(req, res, { message: "Successfully stored" })
+        } catch (error) {
+            next(error)
         }
+
+    }
+
+    static async getQuestionBank(req: Request, res: Response, next: NextFunction) {
+        try {
+            const roles = req.user.role;
+            if (roles.length === 1 && roles.includes(IRoles.SME)) {
+                const data = await QuestionBankUtils.getQuestionBank();
+                sendSuccessResponse(req, res, { data })
+            } else {
+                sendSuccessResponse(req, res, { message: "Yet to be implemented" })
+            }
+        } catch (error) {
+            next(error)
+        }
+    }
 }
--- src/features/v1/questionBank/questionBank.utils.ts
+++ src/features/v1/questionBank/questionBank.utils.ts
@@ -1,5 +1,46 @@
 import { AppDataSource } from "../../../data-source";
+import { jsonParser } from "../../../helpers/common.helpers";
 import { QuestionBank } from "./questionBank.model";
+
+// import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm"
+// @Entity()
+// export class QuestionBank {
+
+//     @PrimaryGeneratedColumn()
+//     id: number
+
+//     @Column()
+//     label: string
+
+//     @Column()
+//     marks: number
+
+//     @Column()
+//     questionContent:string
+
+//     @Column()
+//     questionIndex:string
+
+//     @Column()
+//     subject:number
+
+//     @Column()
+//     topic:string
+
+//     @Column()
+//     type:string
+
+//     @Column()
+//     userId:number
+
+//     @CreateDateColumn()
+//     createdAt: Date;
+
+//     @UpdateDateColumn()
+//     updatedAt: Date;
+
+// }
+
 
 const questionBankRepository = AppDataSource.getRepository(QuestionBank);
 
@@ -7,6 +48,45 @@ export default class QuestionBankUtils {
 
     static async saveQuestionBank(data: QuestionBank[]) {
         return await questionBankRepository.save(data);
+    }
+
+    static async getQuestionBank(userId: number) {
+        // get the subjects from assign_sme for the user and then get the questions from question bank for the subjects group by subject
+        const query = `
+    SELECT 
+        assign_sme.subjectId,
+        JSON_ARRAYAGG(
+            JSON_OBJECT(
+                'id', question_bank.id,
+                'label', question_bank.label,
+                'marks', question_bank.marks,
+                'questionContent', question_bank.questionContent,
+                'questionIndex', question_bank.questionIndex,
+                'subject', question_bank.subject,
+                'topic', question_bank.topic,
+                'type', question_bank.type,
+                'userId', question_bank.userId,
+                'subjectName', subject.name
+            )
+        ) AS questionBankData
+    FROM 
+        assign_sme
+    LEFT JOIN 
+        subject ON assign_sme.subjectId = subject.id
+    LEFT JOIN 
+        question_bank ON subject.id = question_bank.subjectId
+    LEFT JOIN 
+        user ON question_bank.userId = user.id
+    WHERE 
+        assign_sme.userId = ${userId} 
+    GROUP BY 
+        assign_sme.subjectId;
+`;
+
+        const data = await questionBankRepository.query(query);
+        return jsonParser(data);
+
+
     }
 
 
